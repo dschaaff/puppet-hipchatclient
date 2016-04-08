d97c529b3f165c5a8da2a718554563a4098b6b0a
Daniel Schaaff <dschaaff@intellisis.com>
2016-04-08 10:37:28 -0700

    fix boilerplate in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,14 @@
 # == Class: hipchatclient
 #
-# Full description of class hipchatclient here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { hipchatclient:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
+# include hipchatclient
+# 
 # === Authors
 #
-# Author Name <author@domain.com>
+# Author Name <daniel@danielschaaff.com.com>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Daniel Schaaff, unless otherwise noted.
 #
 class hipchatclient (
   $manage_repo = true,
